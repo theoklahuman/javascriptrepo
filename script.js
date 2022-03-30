b96acf3598426d3bc5d50ea9f69e904f973e2e24
theoklahuman <theoklahuman@gmail.com>
2022-03-30 01:32:06 -0500

    Add function to play a round of game

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hello world!");
+
+function computerPlay() { 
+    let roll = Math.floor((Math.random() * 3)) + 1;
+let value;
+if (roll == 1) { value = "rock";
+} else if (roll == 2) { 
+    value = "paper";
+} else { 
+    value = "scissors";
+};
+    return value 
+};
+
